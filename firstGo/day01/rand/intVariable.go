ee45cefcefd847825aad6858a0f0bccc65145bc3
YangYoung <YangYoung2021@163.com>
2022-09-04 22:28:56 +0800

    修改

--- firstGo/day01/rand/intVariable.go
+++ firstGo/day01/rand/intVariable.go
@@ -1,9 +1,10 @@
-package main
+package rand
 
 import "fmt"
 
-func main() {
+func f2() {
 	var a, b int = 10, 20 // 同时声明多个类型相同的变量
-	a, b = b, a // 交换两个变量的值
+	a, b = b, a           // 交换两个变量的值
+
 	fmt.Printf("a的值为: %d, b的值为: %d\n", a, b) // 格式化输出
 }
---
+++ firstGo/day01/type/类型转换.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 100
+	var c1 = rune(a)
+	// go语言中的类型转换是使用类型名称然后使用括号包裹要转换的变量
+	// 这个和Java中的类型转换刚好相反
+	// go中的字符类型占用4个字节，不会显示出定义的rune类型，而是展示int32
+	fmt.Printf("c1 type is %T and a is %c\n", c1, c1)
+	var b byte = 1
+	// byte的类型打印出来时int8，一个字节
+	fmt.Printf("b type is %T\n", b)
+}
