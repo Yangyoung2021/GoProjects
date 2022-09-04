0b93faf447b346c84e08e7aea2ccf9ed2ed0382b
YangYoung <YangYoung2021@163.com>
2022-09-04 15:25:36 +0800

    修改目录结构

---
+++ firstGo/day01/main/Hello.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+    "fmt"
+    "calc"
+)
+
+func main() {
+    fmt.Println("Hello world")
+
+    r1 := calc.Add(10, 20)
+
+    fmt.Println("10 + 20 等于", r1)
+
+    r2 := calc.Sub(10, 20)
+
+    fmt.Println("10 - 20 等于", r2)
+
+    r3 := calc.Multiply(10, 20)
+
+    fmt.Println("10 * 20 等于", r3)
+
+    r4 := calc.Divide(10, 20)
+
+    fmt.Println("10 / 20 等于", r4)
+}
---
+++ firstGo/day01/main/var_auto.go
@@ -0,0 +1,11 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	var a = 100
+	fmt.Print("这是一个普通的打印函数\n")
+	fmt.Println("a的值为: ", a)
+}
