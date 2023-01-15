57a78bc79f393651f9ae639b970790024a903c19
xiongyangyang <xiongyangyang@aspirecn.com>
2023-01-15 12:28:45 +0800

    修改type的注释和打印方式

--- study/day01/type/foreach.go
+++ study/day01/type/foreach.go
@@ -4,13 +4,14 @@ import "fmt"
 
 func main() {
 	var str = "abcdefg"
-
+	// 循环的遍历方式一，使用索引遍历
 	for i := 0; i < len(str); i++ {
-		fmt.Printf("str[%d] = %c\n", i, str[i])
+		fmt.Printf("str[%d] = %c\t", i, str[i])
 	}
-
+	fmt.Println()
+	// 遍历方式二，使用增强for循环
 	for index, data := range str {
-		fmt.Printf("str[%d] = %c\n", index, data)
+		fmt.Printf("str[%d] = %c\t", index, data)
 	}
 
 }
